2d82595490f50b1856d1e5c2683a387fd69b8585
Jose Tafur <j-osea-nt@hotmail.com>
2019-06-30 21:17:41 -0500

    adding project details

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,1 +1,22 @@
+import React from 'react';
 
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
+
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title - {id}</span>
+          <p>Details</p>
+        </div>
+        <div className="card-action gret ligthen-4 grey-text">
+          <div>Posted by Author</div>
+          <div>Fecha</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
