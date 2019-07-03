db573aae40846f66865b56d6b9f95528e950d96c
Jose Tafur <j-osea-nt@hotmail.com>
2019-07-02 21:57:45 -0500

    updating project component with redux

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'this is a title example', content: 'this is a content example'},
+    {id: '2', title: 'this is a title example', content: 'this is a content example'},
+    {id: '3', title: 'this is a title example', content: 'this is a content example'},
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state;
