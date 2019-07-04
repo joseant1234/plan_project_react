89e3b857a7cd4d87d8c101596458614191bccc2f
Jose Tafur <j-osea-nt@hotmail.com>
2019-07-03 22:44:35 -0500

    adding env

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: ENV['APIKEY'],
-  authDomain: "planprojectreact.firebaseapp.com",
-  databaseURL: "https://planprojectreact.firebaseio.com",
-  projectId: "planprojectreact",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId:  process.env.PROJECT_ID,
   storageBucket: "",
   messagingSenderId: "941829892019",
-  appId: ENV['APPID']
+  appId:  process.env.APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
