f7953425e91ed5f95f0c2e6d7ba51ffa5a9150dd
Jose Tafur <j-osea-nt@hotmail.com>
2019-07-05 21:34:18 -0500

    fixing env

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId:  process.env.PROJECT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
   storageBucket: "",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId:  process.env.APP_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
